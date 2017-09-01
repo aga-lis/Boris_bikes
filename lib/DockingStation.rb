a7aca0eace50560704f6808787dc134d02458d81
Agnieszka Lis <agnieszkalis@gmail.com>
2017-09-01 16:34:53 +0100

    adding attr_reader and @variable to DockingStation.rb testing for both in DockingStation_spec.rb

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,6 +1,13 @@
 require_relative './bike'
 class DockingStation
+  attr_reader :bike
+  def initialize
+    @bike = bike
+  end
   def release_bike
     Bike.new
   end
+  def dock(bike)
+    @bike = bike
+end
 end
