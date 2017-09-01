0d978ea8507c8775ed3c3cad2a4b605df8eedf09
keith(mac) <ka33145@googlemail.com>
2017-09-01 15:33:33 +0100

    Check bike working in Docking Station and updated bike and DockingStation

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,4 +3,8 @@ RSpec.describe DockingStation do
   it 'check realise bike_exist' do
    is_expected.to respond_to(:release_bike)
   end
+  it 'check bike working' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
